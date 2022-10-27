5787e6b2dd240115ce397418d517f6505abbf412
Aaditya Raj <aadityaraj085@gmail.com>
2022-10-27 19:12:26 +0530

    dist between thumb and index finger

--- createmarks.py
+++ createmarks.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import mediapipe as mp
+from math import hypot
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -8,27 +9,39 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 
-
-
-
-
 def CreateMarks(cap):
-	with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-		success, image = cap.read()
-		
-		image.flags.writeable = False
-		image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-		results = hands.process(image)
+    with mp_hands.Hands(
+        model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as hands:
+        success, image = cap.read()
 
-		    
-		image.flags.writeable = True
-		image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-		if results.multi_hand_landmarks:
-			for hand_landmarks in results.multi_hand_landmarks:
-				mp_drawing.draw_landmarks(
-					image,
-					hand_landmarks,
-					mp_hands.HAND_CONNECTIONS,
-					mp_drawing_styles.get_default_hand_landmarks_style(),
-					mp_drawing_styles.get_default_hand_connections_style())
-	return image
+        image.flags.writeable = False
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        image.flags.writeable = True
+        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+        landmarkList = []
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for _id, landm in enumerate(hand_landmarks.landmark):
+                    h, w, c = image.shape
+                    x, y = int(landm.x * w), int(landm.y * h)
+
+                    # if(hand_landmarks!=0):
+                    # if id==8:
+                    # cv.circle(image,(cx,cy), 10 , (255,0,255), cv.FILLED)
+
+                    landmarkList.append([_id, x, y])
+        if landmarkList != []:
+            x_1, y_1 = landmarkList[4][1], landmarkList[4][2]
+            x_2, y_2 = landmarkList[8][1], landmarkList[8][2]
+            cv.circle(image, (x_1, y_1), 7, (255, 255, 255), cv.FILLED)
+            cv.circle(image, (x_2, y_2), 7, (255, 255, 255), cv.FILLED)
+            distance = hypot(x_2 - x_1, y_2 - y_1)
+            if distance >= 0 and distance < 20:
+                print("Draw Now")
+            else:
+                print("Not draw")
+
+    return image
