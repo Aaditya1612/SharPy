b6341dca8235bc71c3e67cdb0222979ab515c804
Aaditya Raj <aadityaraj085@gmail.com>
2022-10-29 22:13:32 +0530

    beautified sharpy.py

--- sharpy.py
+++ sharpy.py
@@ -9,7 +9,6 @@ h = int(cap.get(3))
 w = int(cap.get(4))
 print(h, w)
 
-# cord_blue = {'x':
 
 img = cv.imread("./window_layout_bg.png", 1)
 resize_img = cv.resize(img, (640, 480))
@@ -27,19 +26,23 @@ green_index = 0
 red_index = 0
 yellow_index = 0
 
+
 colorIndex = 0
 
 colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+canvas = None
 
 
 while True:
+
+    xp, yp = 0, 0
     ret, frame = cap.read(0)
+    eraserMode = False
 
     frame = cv.flip(frame, 1)
-    frame, isDrawing, center = createmarks.CreateMarks(frame)
-    # print(isDrawing, h-index_x, index_y)
-
-    # Fliping the frame horizontally
+    if canvas is None:
+        canvas = np.zeros_like(frame)
+    frame, isDrawing, center, thumb = createmarks.CreateMarks(frame)
 
     roi = frame[-0 - 0 : h, -w - w : h]
     roi[np.where(mask)] = 0
@@ -63,8 +66,9 @@ while True:
                 colorIndex = 3  # For Yellow
             if 218 <= center[0] <= 253:
                 colorIndex = 1  # For green
-            if 300 < center[0] <= 333:
+            if 300 <= center[0] <= 333:
                 colorIndex = 2  # For red
+
             if 476 <= center[0] <= 512:
                 bpoints = [deque(maxlen=512)]
                 gpoints = [deque(maxlen=512)]
@@ -75,6 +79,7 @@ while True:
                 red_index = 0
                 green_index = 0
                 yellow_index = 0
+
         bpoints.append(deque(maxlen=512))
         blue_index += 1
         gpoints.append(deque(maxlen=512))
@@ -91,6 +96,7 @@ while True:
                 if points[i][j][k - 1] is None or points[i][j][k] is None:
                     continue
                 cv.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 5)
+
     cv.imshow("SharPy", frame)
 
     if cv.waitKey(1) & 0xFF == ord("q"):
