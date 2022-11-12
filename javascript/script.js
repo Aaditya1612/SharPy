df991562313d7766d0760f83063a715bc7cecdd3
Aaditya Raj <aadityaraj085@gmail.com>
2022-11-12 09:07:47 +0530

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function(){stickNav()};
-
-function stickNav(){
-    var navBar = document.getElementById('navbar');
-
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        navBar.style.position ="fixed"
-    }
-}
